70199ad028c2fcc9f6af1df6778ebc56bd00c5e4
Richard edu <richardedu318@gmail.com>
2025-08-28 12:51:47 +0530

    Added site map

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import VitePluginSitemap from "vite-plugin-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,6 +12,9 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    VitePluginSitemap({
+      hostname: 'https://richardbmelath.vercel.app/',
+    }),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
